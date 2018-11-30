8ece15e481d1079dbdf2dd8ba7136db86f72a885
Ilya Kopeshtianski <lupusanay@gmail.com>
2018-11-30 20:10:19 +0300

    Relational paths replaced by absolute

--- auth/tests/base_test_case.py
+++ auth/tests/base_test_case.py
@@ -1,14 +1,15 @@
 from flask import current_app
 from flask_testing import TestCase
 
-from ..app import create_app, db
+from auth.app import create_app, db
 
 
 class BaseTestCase(TestCase):
     def create_app(self):
-        return create_app('app.config.TestingConfig')
+        return create_app('auth.app.config.TestingConfig')
 
     def setUp(self):
+        db.drop_all()
         current_app.logger.info('Creating database')
         db.create_all()
         db.session.commit()
